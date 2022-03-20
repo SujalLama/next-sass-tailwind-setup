8b1bd19dbf17360a33797360f7cbadb0e9b1dd48
Sujal Lama <lsujal41@gmail.com>
2022-03-20 16:39:28 +0545

    next, sass and tailwind setup

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import styles from "/styles/pages/home.module.scss";
+
+const Index = () => {
+  return (
+    <h1 className={`${styles.title} underline`}>Hello world</h1>
+  )
+}
+
+export default Index
